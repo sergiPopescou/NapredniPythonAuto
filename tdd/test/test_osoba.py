f47ffcd087be42893895fea5e592240fd9fa315c
Srdjan Popic <srdjan.popic@gmail.com>
2023-09-13 09:36:40 +0200

    pocetak treceg dana testovi

---
+++ tdd/test/test_osoba.py
@@ -0,0 +1,33 @@
+import pytest
+from modules.osoba import Osoba
+
+imena_osoba = [
+    ("Ljuba", "Alicic"),
+    ("Shemsa", "Suljakovic"),
+    ("Toma", "Zdravkovic")
+]
+
+@pytest.mark.parametrize("ime,prezime", imena_osoba, ids=range(len(imena_osoba)))
+def test_ime_prezime_01(ime, prezime):
+    """
+    Requirement: 01
+    Osoba je definisana imenom i prezimenom
+    Mocking:
+    --------
+        -
+    :return:
+    """
+    o = Osoba(ime, prezime)
+    assert hasattr(o, "ime")
+    assert hasattr(o, "prezime")
+    assert o.ime == ime
+    assert o.prezime == prezime
+
+@pytest.mark.parametrize("ime,prezime", imena_osoba, ids=range(len(imena_osoba)))
+def test_osoba_str(ime, prezime):
+    """
+    Requirement 02:
+    Osoba ima str
+    """
+    o = Osoba(ime, prezime)
+    assert str(o) == f"{ime} {prezime}"
