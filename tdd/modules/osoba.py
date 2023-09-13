22dc1d9ea59adc3f098d8b1125725bce4635206b
Srdjan Popic <srdjan.popic@gmail.com>
2023-09-13 13:00:37 +0200

    prvi test

---
+++ tdd/modules/osoba.py
@@ -0,0 +1,11 @@
+class Osoba:
+    def __init__(self, ime, prezime):
+        """
+
+        usage:
+            >>> Osoba("toma", "zdravkovic") == 1
+        :param ime:
+        :param prezime:
+        """
+        self.ime = ime
+        self.prezime = prezime
--- tdd/test/test_osoba.py
+++ tdd/test/test_osoba.py
@@ -1,5 +1,5 @@
 import pytest
-from modules.osoba import Osoba
+from tdd.modules.osoba import Osoba
 
 imena_osoba = [
     ("Ljuba", "Alicic"),
@@ -22,12 +22,3 @@ def test_ime_prezime_01(ime, prezime):
     assert hasattr(o, "prezime")
     assert o.ime == ime
     assert o.prezime == prezime
-
-@pytest.mark.parametrize("ime,prezime", imena_osoba, ids=range(len(imena_osoba)))
-def test_osoba_str(ime, prezime):
-    """
-    Requirement 02:
-    Osoba ima str
-    """
-    o = Osoba(ime, prezime)
-    assert str(o) == f"{ime} {prezime}"
